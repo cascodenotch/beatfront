29b73c30a11177f4dadc64ae2eb99ffcf346e407
e-nihilus <epardorayo@gmail.com>
2024-12-09 14:45:20 +0100

    pagina y tarjetas Sets

--- src/app/component/tarjeta-set/tarjeta-set.component.ts
+++ src/app/component/tarjeta-set/tarjeta-set.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./tarjeta-set.component.css']
 })
 export class TarjetaSetComponent {
-
+  cards = [
+    { title: 'Título Set 1' },
+    { title: 'Título Set 2' },
+    { title: 'Título Set 3' },
+    { title: 'Título Set 4' },
+    { title: 'Título Set 5' },
+    { title: 'Título Set 6' },
+    { title: 'Título Set 7' },
+    { title: 'Título Set 8' },
+    { title: 'Título Set 9' },
+  ];
 }
