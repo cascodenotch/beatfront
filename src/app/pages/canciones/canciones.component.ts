94e3bf2093e56eda3aaafb76d4e9a6e98c97c60d
e-nihilus <epardorayo@gmail.com>
2024-12-10 12:54:09 +0100

    tarjeta y pagina canciones

--- src/app/pages/canciones/canciones.component.ts
+++ src/app/pages/canciones/canciones.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class CancionesComponent {
 
+  searchText: string = '';
+  danceability: string = '';
+  energy: string = '';
+  key: string = '';
+  tempo: string = '';
 }
