33e0fda69c0e1b52df57d0b97ceacf8defc205b8
e-nihilus <epardorayo@gmail.com>
2024-12-11 16:16:56 +0100

    nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MisSetsComponent } from './pages/mis-sets/mis-sets.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { EditarSetComponent } from './pages/editar-set/editar-set.component';
+import { AnalisisComponent } from './pages/analisis/analisis.component';
+import { CancionComponent } from './models/cancion/cancion.component';
+import { NostrosComponent } from './pages/nostros/nostros.component';
+import { RecomendacionesComponent } from './pages/recomendaciones/recomendaciones.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: "landing", component: LandingComponent },
   { path: "mis-sets", component: MisSetsComponent },
+  { path: "edit-set", component:EditarSetComponent},
+  { path: "analisis", component:AnalisisComponent},
+  { path: "canciones", component:CancionComponent},
+  { path: "nosotros", component:NostrosComponent},
+  { path: "recomend", component:RecomendacionesComponent}
 ];
 
 @NgModule({
