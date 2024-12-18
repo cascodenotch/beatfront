446e0445f6b54e346b468528dcbfcbe7f219f5c8
e-nihilus <epardorayo@gmail.com>
2024-12-18 16:35:53 +0100

    cambios de songs

--- src/app/component/tarjeta-cancion/tarjeta-cancion.component.ts
+++ src/app/component/tarjeta-cancion/tarjeta-cancion.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SongsService } from '../../shared/songs.service';
+
 
 @Component({
   selector: 'app-tarjeta-cancion',
@@ -11,6 +14,31 @@ export class TarjetaCancionComponent {
   @Input() energy: string = ''; 
   @Input() key: string = ''; 
   @Input() tempo: string = ''; 
+  songs: any[] = [];
+
+  constructor(private route: ActivatedRoute, private songService: SongsService) {}
+
+  ngOnInit(): void {
+    // Obtener el token de la URL a través del servicio
+    const token = this.songService.tokenUser;  // Verifica que el token esté disponible en el servicio
+
+    if (token) {
+      console.log("Token recibido en canciones:", token);
+
+      // Llamar al servicio para obtener las canciones
+      this.songService.getSavedSongs(token).subscribe(
+        (data: any) => {
+          console.log('Canciones obtenidas:', data);
+          this.songs = data;  // Asignar las canciones al arreglo 'songs'
+        },
+        (error) => {
+          console.error('Error al obtener canciones:', error);
+        }
+      );
+    } else {
+      console.error("Token no encontrado");
+    }
+  }
 
   // cards = [
   //   { title: 'Blinding Lights', autor: 'The Weeknd', danceability: 8, energy: 7, key: 'F#m', tempo: '171', duracion: '3:20' },
@@ -27,35 +55,26 @@ export class TarjetaCancionComponent {
   //   { title: 'Happier Than Ever', autor: 'Billie Eilish', danceability: 5, energy: 6, key: 'E', tempo: '76', duracion: '4:58' },
   // ];
 
-  cards = [
-    { title: 'Blinding Lights', autor: 'The Weeknd', danceability: 8, energy: 7, key: 'F#m', tempo: '171', duracion: '3:20' },
-    { title: 'Shape of You', autor: 'Ed Sheeran', danceability: 8, energy: 6, key: 'C#m', tempo: '96', duracion: '3:53' },
-    { title: 'Levitating', autor: 'Dua Lipa', danceability: 9, energy: 8, key: 'Bm', tempo: '103', duracion: '3:23' },
-    { title: 'Uptown Funk', autor: 'Mark Ronson ft. Bruno Mars', danceability: 8, energy: 9, key: 'Dm', tempo: '115', duracion: '4:30' },
-    { title: 'Sunflower', autor: 'Post Malone & Swae Lee', danceability: 9, energy: 7, key: 'A', tempo: '90', duracion: '2:38' },
-    { title: 'Happier Than Ever', autor: 'Billie Eilish', danceability: 5, energy: 6, key: 'E', tempo: '76', duracion: '4:58' },
-];
 
-
-  filteredCards = this.cards; 
+  // filteredCards = this.cards; 
 
   
-  ngOnChanges(): void {
-    this.filteredCards = this.cards.filter((card) => {
-      const matchesSearchText = this.searchText ? card.title.toLowerCase().includes(this.searchText.toLowerCase()) ||
-          card.autor.toLowerCase().includes(this.searchText.toLowerCase()) : true;
+  // ngOnChanges(): void {
+  //   this.filteredCards = this.cards.filter((card) => {
+  //     const matchesSearchText = this.searchText ? card.title.toLowerCase().includes(this.searchText.toLowerCase()) ||
+  //         card.autor.toLowerCase().includes(this.searchText.toLowerCase()) : true;
 
-      const matchesDanceability = this.danceability ? card.danceability === parseInt(this.danceability, 10) : true;
+  //     const matchesDanceability = this.danceability ? card.danceability === parseInt(this.danceability, 10) : true;
 
-      const matchesEnergy = this.energy ? card.energy === parseInt(this.energy, 10) : true;
+  //     const matchesEnergy = this.energy ? card.energy === parseInt(this.energy, 10) : true;
 
-      const matchesKey = this.key ? card.key.toLowerCase() === this.key.toLowerCase() : true;
+  //     const matchesKey = this.key ? card.key.toLowerCase() === this.key.toLowerCase() : true;
 
-      const matchesTempo = this.tempo ? card.tempo.toLowerCase() === this.tempo.toLowerCase(): true;
+  //     const matchesTempo = this.tempo ? card.tempo.toLowerCase() === this.tempo.toLowerCase(): true;
 
-      return matchesSearchText && matchesDanceability && matchesEnergy && matchesKey && matchesTempo;
-    });
-  }
+  //     return matchesSearchText && matchesDanceability && matchesEnergy && matchesKey && matchesTempo;
+  //   });
+  // }
 
   showValidation = false; 
 
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,6 @@
 export class User {
     constructor(
+      public id_user: number,
       public email: string,
       public photo: string,
       public id_spotify: string,
